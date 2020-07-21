913bc23e631d9fadb061e4747b814dbe1dffbdff
Ludvig Lind <ludviglind@protonmail.com>
2020-07-21 10:31:48 +0200

    model object

--- game.js
+++ game.js
@@ -18,9 +18,34 @@ let model = {
     numShips: 3,
     shipLength: 3,
     shipsSunk: 0,
-
-    ships: [
-        {locations: ["06", "16", "26"], hits: ["", "", ""] },
-        {locations: ["24", "34", "44"], hits: ["", "", ""] },
-        {locations: ["10", "11", "12"], hits: ["", "", ""] }]
+    ships: [{locations: ["06", "16", "26"], hits: ["", "", ""] },
+            {locations: ["24", "34", "44"], hits: ["", "", ""] },
+            {locations: ["10", "11", "12"], hits: ["", "", ""] }],
+    fire: function(guess) {
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = this.ships[i];
+            let index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                if (this.isSunk(ship)) {
+                    view.displayMessage("You sank my battleship!");
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You missed.");
+        return false;
+    },
+    isSunk: function(ship) {
+        for (let i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false
+            }
+        }
+        return true;
+    }
 };
